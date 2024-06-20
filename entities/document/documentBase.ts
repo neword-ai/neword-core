09a899c9fb7c6234818f131e262b6be3dcab49bc
shaidrai <shai@makat.ai>
2024-06-20 11:53:49 +0300

    init

--- entities/document/documentBase.ts
+++ entities/document/documentBase.ts
@@ -1,7 +1,9 @@
 import { DocumentEntityType } from "../../types/DocumentEntityType";
+import { DocumentCategory } from "../../types/documentCategory";
 
 export interface DocumentBase<InputParams, OutputBlock> {
   type: DocumentEntityType;
+  category: DocumentCategory;
   inputParams: InputParams;
   outputBlock: OutputBlock[];
 }
---
+++ types/documentCategory.ts
@@ -0,0 +1,5 @@
+export enum DocumentCategory {
+  SOCIAL_MEDIA = "SOCIAL_MEDIA",
+  WEB = "WEB",
+  EMAIL = "EMAIL",
+}
