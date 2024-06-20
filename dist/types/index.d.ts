874eab0c740e96e5eea218cbb0e42905cb7b1f26
shaidrai <shai@makat.ai>
2024-06-20 13:11:00 +0300

    f

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,2 +1,4 @@
-export type Shai = string;
+export * from "./types/documentCategory";
+export * from "./types/DocumentEntityType";
+export * from "./entities/document/documentBase";
 //# sourceMappingURL=index.d.ts.map
