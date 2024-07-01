c4c0e69da0c90ed2f2ee1020843d1313998e8730
shaidrai <shai@makat.ai>
2024-07-01 14:23:53 +0300

    f

---
+++ src/entities/document/documentEntity.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+import { DocumentEntityType } from "../../types/DocumentEntityType";
+import { DocumentTypeFactory } from "./documentBase";
+import { EntityBase } from "../entityBase";
+
+export interface DocumentEntity extends EntityBase {
+  type: DocumentEntityType;
+  output: z.infer<(typeof DocumentTypeFactory)[DocumentEntityType]["output"]>;
+  inputParams: z.infer<
+    (typeof DocumentTypeFactory)[DocumentEntityType]["inputParams"]
+  >;
+  websiteId: string;
+  companyId: string;
+}
