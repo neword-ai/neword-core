33d3eb04fd1bf84abea9340ee0ed805374ecbb39
shaidrai <shai@makat.ai>
2024-07-01 14:42:06 +0300

    f

--- src/entities/document/documentEntity.ts
+++ src/entities/document/documentEntity.ts
@@ -3,12 +3,17 @@ import { DocumentEntityType } from "../../types/DocumentEntityType";
 import { DocumentTypeFactory } from "./documentBase";
 import { EntityBase } from "../entityBase";
 
-export interface DocumentEntity extends EntityBase {
+export interface DocumentEntity<T extends DocumentEntityType>
+  extends EntityBase {
   type: DocumentEntityType;
-  output: z.infer<(typeof DocumentTypeFactory)[DocumentEntityType]["output"]>[];
-  inputParams: z.infer<
-    (typeof DocumentTypeFactory)[DocumentEntityType]["inputParams"]
-  >;
+  output: z.infer<(typeof DocumentTypeFactory)[T]["output"]>[];
+  inputParams: z.infer<(typeof DocumentTypeFactory)[T]["inputParams"]>;
   websiteId: string;
   companyId: string;
 }
+
+// function get<T extends DocumentEntityType>(): DocumentEntity<T> {
+//   return {} as any as DocumentEntity<T>;
+// }
+
+// const x = get<DocumentEntityType.INSTAGRAM_CAPTION>().inputParams.;
