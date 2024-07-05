764bdc7be17468e9d4aa51ca46e3b876ff41c67b
shai drai <shaidrai@shais-Laptop.lan>
2024-07-05 22:55:52 +0300

    main - d

--- src/entities/document/documentBase.ts
+++ src/entities/document/documentBase.ts
@@ -2,9 +2,14 @@ import { DocumentEntityType } from "../../types/DocumentEntityType";
 import { z } from "zod";
 import { DocumentCategory } from "../../types/documentCategory";
 
+const baseInputParams = z.object({
+  description: z.string(),
+  imageUrl: z.string().optional(),
+});
+
 export const DocumentTypeFactory = {
   [DocumentEntityType.INSTAGRAM_CAPTION]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       caption: z.string(),
     }),
     output: z.object({
@@ -13,43 +18,35 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.INSTAGRAM_POST_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       ideaDescription: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.INSTAGRAM_BIO_DESCRIPTION]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       bioDescription: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.INSTAGRAM_STORY_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       storyIdea: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.INSTAGRAM_REEL_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       reelIdea: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.INSTAGRAM_REEL_SCRIPT]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       script: z.string(),
     }),
     output: z.object({
@@ -58,7 +55,7 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.FACEBOOK_POST_CAPTION]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       caption: z.string(),
     }),
     output: z.object({
@@ -67,25 +64,21 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.FACEBOOK_POST_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       postIdea: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.FACEBOOK_PAGE_ABOUT]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       pageAbout: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.LINKEDIN_POST_CAPTION]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       caption: z.string(),
     }),
     output: z.object({
@@ -94,25 +87,21 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.LINKEDIN_POST_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       postIdea: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.LINKEDIN_PAGE_ABOUT]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       pageAbout: z.string(),
     }),
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.TWITTER_TWIT]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       tweet: z.string(),
     }),
     output: z.object({
@@ -121,7 +110,7 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.THREADS_TWIT]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       thread: z.string(),
     }),
     output: z.object({
@@ -130,9 +119,7 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.SOCIAL_MEDIA,
   },
   [DocumentEntityType.EMAIL_CONTENT]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       subject: z.string(),
       content: z.string(),
@@ -140,16 +127,14 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.MAILING,
   },
   [DocumentEntityType.EMAIL_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       emailIdea: z.string(),
     }),
     category: DocumentCategory.MAILING,
   },
   [DocumentEntityType.SMS_CONTENT]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       content: z.string(),
     }),
     output: z.object({
@@ -158,16 +143,14 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.MAILING,
   },
   [DocumentEntityType.SMS_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       smsIdea: z.string(),
     }),
     category: DocumentCategory.MAILING,
   },
   [DocumentEntityType.PHONE_CALL_SCRIPT]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       script: z.string(),
     }),
     output: z.object({
@@ -176,7 +159,7 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.MAILING,
   },
   [DocumentEntityType.GOOGLE_ADS_KEYWORDS]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       keywords: z.array(z.string()),
     }),
     output: z.object({
@@ -185,25 +168,21 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.PAID,
   },
   [DocumentEntityType.GOOGLE_ADS_IDEA]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       adIdea: z.string(),
     }),
     category: DocumentCategory.PAID,
   },
   [DocumentEntityType.WEBSITE_ABOUT]: {
-    inputParams: z.object({
-      description: z.string(),
-    }),
+    inputParams: baseInputParams,
     output: z.object({
       aboutContent: z.string(),
     }),
     category: DocumentCategory.WEB,
   },
   [DocumentEntityType.WEBSITE_FAQ]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       question: z.string(),
       answer: z.string(),
     }),
@@ -215,7 +194,7 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.WEB,
   },
   [DocumentEntityType.WEBSITE_TESTEMONIAL]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       testimonial: z.string(),
     }),
     output: z.object({
@@ -224,9 +203,8 @@ export const DocumentTypeFactory = {
     category: DocumentCategory.WEB,
   },
   [DocumentEntityType.WEBSITE_PRODUCT_DESCRIPTION]: {
-    inputParams: z.object({
+    inputParams: baseInputParams.extend({
       productName: z.string(),
-      description: z.string(),
     }),
     output: z.object({
       productDescriptions: z.array(
