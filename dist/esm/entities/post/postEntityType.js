f1456be3e260d74b220f56de68fb558c5b0d3127
sagi1231 <sagigi2211@gmail.com>
2024-09-27 16:06:14 +0300

    s

--- dist/esm/entities/post/postEntityType.js
+++ dist/esm/entities/post/postEntityType.js
@@ -3,7 +3,7 @@ export var PostEntityType;
     PostEntityType["INSTAGRAM_POST"] = "INSTAGRAM_POST";
     PostEntityType["FACEBOOK_POST"] = "FACEBOOK_POST";
     PostEntityType["LINKEDIN_POST"] = "LINKEDIN_POST";
-    PostEntityType["TWITTER_TWIT"] = "TWITTER_TWEET";
+    PostEntityType["TWITTER_TWIT"] = "TWITTER_TWIT";
     PostEntityType["THREADS_TWIT"] = "THREADS_TWIT";
     PostEntityType["TIKTOK_POST"] = "TIKTOK_POST";
     PostEntityType["EMAIL"] = "EMAIL";
